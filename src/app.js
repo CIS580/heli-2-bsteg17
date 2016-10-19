7d05592eedd85360f653b98e3ba3f7bd3ada0a54
bsteg17 <bsteg17@gmail.com>
2016-10-19 15:33:35 -0500

    implemented bullet pool

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@
 /* Classes */
 const Game = require('./game');
 const Vector = require('./vector');
+const BulletPool = require('./bullet_pool');
 
 /* Global variables */
 var canvas = document.getElementById('screen');
@@ -39,7 +40,7 @@ var reticule = {
   x: 0,
   y: 0
 }
-
+var bullets = new BulletPool(10);
 /**
  * @function onmousemove
  * Handles mouse move events
@@ -59,6 +60,7 @@ window.onmousedown = function(event) {
   reticule.x = event.offsetX;
   reticule.y = event.offsetY;
   // TODO: Fire bullet in direction of the retciule
+  bullets.add(player.position, {x:1, y:0});
 }
 
 /**
@@ -184,6 +186,9 @@ function update(elapsedTime) {
 
   if(camera.x < 0) camera.x = 0;
 
+  bullets.update(elapsedTime, function(bullet) {
+    return bullet.y < 0;	  
+  });
 }
 
 /**
@@ -216,6 +221,8 @@ function render(elapsedTime, ctx) {
   ctx.rotate(player.angle);
   ctx.drawImage(player.img, 0, 0, 131, 53, -60, 0, 131, 53);
   ctx.restore();
+
+  bullets.render(elapsedTime, ctx);
 
   // Render the reticule
   ctx.save();
